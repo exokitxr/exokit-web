0a1c5f7073be4ae73e2e0b02741691a5caa9f699
Avaer Kazmer <a@modules.io>
2019-08-08 19:25:11 -0400

    Add compiled modules

---
+++ modules/parse-int.js
@@ -0,0 +1,14 @@
+const exports = {};
+const module = {exports};
+
+var parcelRequire = function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"+nDy":[function(require,module,exports) {
+"use strict";module.exports=Number.isNaN||function(e){return e!=e};
+},{}],"aWvv":[function(require,module,exports) {
+"use strict";var e=require("number-is-nan");module.exports=Number.isFinite||function(r){return!("number"!=typeof r||e(r)||r===1/0||r===-1/0)};
+},{"number-is-nan":"+nDy"}],"X8oG":[function(require,module,exports) {
+var e=require("is-finite");module.exports=Number.isInteger||function(r){return"number"==typeof r&&e(r)&&Math.floor(r)===r};
+},{"is-finite":"aWvv"}],"Focm":[function(require,module,exports) {
+"use strict";var e=require("is-integer"),t=/^-?\d+$/;module.exports=function(r){return"number"==typeof r?e(r)?r:void 0:"string"==typeof r&&t.test(r)?parseInt(r,10):void 0};
+},{"is-integer":"X8oG"}]},{},["Focm"], null)
+
+export default module.exports;
