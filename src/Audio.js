763e48d6c119424d37ebcc7a0fe0ee4090bc0b44
Avaer Kazmer <a@modules.io>
2019-08-09 16:43:51 -0400

    Add Audio.js

---
+++ src/Audio.js
@@ -0,0 +1,25 @@
+class AudioContext {}
+class AudioNode {}
+class AudioBufferSourceNode {}
+class OscillatorNode {}
+class AudioDestinationNode {}
+class AudioParam {}
+class AudioListener {}
+class GainNode {}
+class AnalyserNode {}
+class PannerNode {}
+class StereoPannerNode {}
+
+export {
+  AudioContext,
+  AudioNode,
+  AudioBufferSourceNode,
+  OscillatorNode,
+  AudioDestinationNode,
+  AudioParam,
+  AudioListener,
+  GainNode,
+  AnalyserNode,
+  PannerNode,
+  StereoPannerNode,
+};
