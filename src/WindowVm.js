3f988fddaa104e1fb45851a69936915e931ed39b
Avaer Kazmer <a@modules.io>
2019-09-01 22:17:01 -0400

    WindowVm.js dead code cleanup

--- src/WindowVm.js
+++ src/WindowVm.js
@@ -1,8 +1,5 @@
 import path from '../modules/path-browserify.js';
 import GlobalContext from './GlobalContext.js';
-
-/* import utils from './utils.js';
-const {_getProxyUrl} = utils; */
  
 // const iframeSrc = `${import.meta.url.replace(/[^\/]+$/, '')}iframe.html`;
 const entrypointSrc = `${import.meta.url.replace(/[^\/]+$/, '')}WindowBase.js`;
