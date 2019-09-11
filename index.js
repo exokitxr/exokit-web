92f8520ce23f87b04c39d3071a0c0b0c62ee1d78
Avaer Kazmer <a@modules.io>
2019-09-11 19:20:49 -0400

    Output url in index.js

--- index.js
+++ index.js
@@ -5,15 +5,13 @@ const http = require('http');
 const https = require('https');
 const express = require('express');
 
-const PORT = process.env['PORT'] || 80;
-
 const app = express();
 app.use(express.static(__dirname));
 
-http.createServer(app)
-  .listen(PORT);
 https.createServer({
   key: fs.readFileSync('cert/privkey.pem'),
   cert: fs.readFileSync('cert/fullchain.pem'),
 }, app)
   .listen(443)
+
+console.log(`https://dev.exokit.org`);
