a48d758f053e6ec57d222985770f7eadcdafe045
Avaer Kazmer <a@modules.io>
2019-09-18 00:34:06 -0400

    Add initial xr-site.js

---
+++ src/xr-site.js
@@ -0,0 +1,12 @@
+class XRSite extends HTMLElement {
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+  }
+  disconnectedCallback() {
+  }
+}
+customElements.define('xr-site', XRSite);
+
+export default XRSite;
