34e8017908ec70984cef059926527b7b73d917e1
Avaer Kazmer <a@modules.io>
2019-09-18 05:38:05 -0400

    Major xr-site implementation work

--- src/xr-site.js
+++ src/xr-site.js
@@ -1,10 +1,54 @@
+import XRIFrame from './xr-iframe.js';
+
+import GlobalContext from './GlobalContext.js';
+
+import utils from './utils.js';
+const {_makeNullPromise} = utils;
+
 class XRSite extends HTMLElement {
   constructor() {
     super();
   }
-  connectedCallback() {
+  async connectedCallback() {
+    console.log('connected 1', this);
+
+    this.fakeXrDisplay = new FakeXRDisplay();
+    this.fakeXrDisplay.pushUpdate();
+    this.fakeXrDisplay.enable();
+
+    this.session = null;
+    this.width = this.fakeXrDisplay.width;
+    this.height = this.fakeXrDisplay.height;
+    this.customLayers = [];
+    this.sessionPromise = _makeNullPromise();
+
+    this.observer = new MutationObserver(async mutations => {
+      await GlobalContext.loadPromise;
+
+      this.session.layers = Array.from(this.childNodes)
+        .filter(childNode => childNode instanceof XRIFrame)
+        .concat(this.customLayers);
+      // console.log('update layers', Array.from(this.childNodes), XRIFrame, this.session.layers);
+    });
+    this.observer.observe(this, {
+      childList: true,
+    });
+
+    const session = await navigator.xr.requestSession({
+      exclusive: true,
+    });
+    // session.layers = [];
+    this.session = session;
+    this.sessionPromise.resolve(session);
   }
   disconnectedCallback() {
+    console.log('disconnected', this);
+  }
+  requestSession() {
+    return this.sessionPromise;
+  }
+  addLayer(layer) {
+    this.customLayers.push(layer);
   }
 }
 customElements.define('xr-site', XRSite);
