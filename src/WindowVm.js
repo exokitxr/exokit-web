8f94f34b3d4e7d2a61f6207b183e7f209275162b
Avaer Kazmer <a@modules.io>
2019-09-19 06:43:09 -0400

    Bugfix WindowVm url mangling hashes

--- src/WindowVm.js
+++ src/WindowVm.js
@@ -40,10 +40,10 @@ class WorkerVm extends EventTarget {
     _getFollowUrl(options.args.options.url)
       .then(followUrl => new Promise((accept, reject) => {
         const src = window.location.origin +
-          !options.args.options.url.startsWith('data:') ?
+          (options.args.options.url.startsWith('data:') ?
             `${window.location.origin}/xr-engine-${Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5)}.html`
           :
-            options.args.options.url.replace(/^[a-z]+:\/\/[a-zA-Z0-9\-\.]+(?::[0-9]+)?/, '');
+            options.args.options.url.replace(/^[a-z]+:\/\/[a-zA-Z0-9\-\.]+(?::[0-9]+)?/, ''));
         const dst = `\
 <!doctype html>
 <html>
