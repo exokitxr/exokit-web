196e58fe8d3fb6ad4f588c7b71cc533760654d94
Avaer Kazmer <a@modules.io>
2019-09-28 22:54:25 -0400

    Dead code cleanup

--- src/Window.js
+++ src/Window.js
@@ -1,5 +1,4 @@
 import path from '../modules/path-browserify.js';
-// import util from '../modules/util.js';
 
 // import {EventTarget, KeyboardEvent, SpatialEvent} from './Event.js';
 import {CanvasRenderingContext2D, WebGLRenderingContext, WebGL2RenderingContext} from './Graphics.js';
